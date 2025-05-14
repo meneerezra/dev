1bfb20f87654ed2e8f8287ae394c9d3f4d69763e
meneerezra <ezraschutte227@gmail.com>
2025-05-15 00:51:50 +0200

    Feat: Init run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
-#!usr/bin/env bash
+#!/bin/bash
+os="NULL" 
+if [ -z $1 ]
+then
+    echo "Provide an argument (linux/mac)"
+elif [ $1 != "mac" ] && [ $1 != "linux" ];
+then
+    echo "Invalid os/argument (linux/mac)"
+    exit 1
+fi
+os=$1
+echo $os
 
 
 
