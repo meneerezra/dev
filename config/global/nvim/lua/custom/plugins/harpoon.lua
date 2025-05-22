7a066720c838b832f4ac63ce57a44cf379973a4b
meneerezra <ezraschutte227@gmail.com>
2025-05-22 19:34:30 +0200

    Add ctrl/cmds mappings for macos
    awdwa

--- config/global/nvim/lua/custom/plugins/harpoon.lua
+++ config/global/nvim/lua/custom/plugins/harpoon.lua
@@ -28,11 +28,11 @@ return {
 		end)
 
 		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<C-S-P>", function()
-			harpoon:list():prev()
-		end)
-		vim.keymap.set("n", "<C-S-N>", function()
-			harpoon:list():next()
-		end)
+		-- vim.keymap.set("n", "<C-S-P>", function()
+		-- 	harpoon:list():prev()
+		-- end)
+		-- vim.keymap.set("n", "<C-S-N>", function()
+		-- 	harpoon:list():next()
+		-- end)
 	end,
 }
